abe59d85b8eb6718734309a574c8a7b6cba8ee7d
zum1k <zumo1988@mail.ru>
2020-10-22 18:02:03 +0300

    add methods to

--- src/main/java/com/epam/esm/repository/impl/AbstractRepository.java
+++ src/main/java/com/epam/esm/repository/impl/AbstractRepository.java
@@ -1,9 +1,35 @@
 package com.epam.esm.repository.impl;
 
 import com.epam.esm.repository.Repository;
+import com.epam.esm.repository.Specification;
 
 import java.sql.Connection;
+import java.util.Comparator;
+import java.util.List;
 
-public abstract class AbstractRepository implements Repository {
+public abstract class AbstractRepository<T> implements Repository<T> {
+    @Override
+    public void add(T t) {
 
+    }
+
+    @Override
+    public void remove(Specification specification) {
+
+    }
+
+    @Override
+    public void update(T t) {
+
+    }
+
+    @Override
+    public List<T> find(Specification specification) {
+        return null;
+    }
+
+    @Override
+    public List<T> sort(Comparator<T> comparator) {
+        return null;
+    }
 }
