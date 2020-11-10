d8431d4e1df1d4cc471201aeca491ea8d7ddec29
zum1k <zumo1988@mail.ru>
2020-11-11 02:37:32 +0300

    add annotations @NotNull etc

--- src/main/java/com/epam/esm/entity/dto/GiftCertificateDto.java
+++ src/main/java/com/epam/esm/entity/dto/GiftCertificateDto.java
@@ -3,6 +3,9 @@ package com.epam.esm.entity.dto;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
@@ -11,12 +14,21 @@ import java.util.List;
 @NoArgsConstructor
 //TODO need validation
 public class GiftCertificateDto {
+
     private long giftId;
+    @NotNull
+    @Size(min = 2, max = 45)
     private String name;
+    @NotNull
+    @Size(min = 2, max = 45)
     private String description;
+    @NotNull
+    @Min(1)
     private BigDecimal price;
     private LocalDate createDate;
     private LocalDate lastUpdateDate;
+    @NotNull
+    @Min(1)
     private long duration;
     private List<TagDto> tags;
 
--- src/main/java/com/epam/esm/entity/dto/TagDto.java
+++ src/main/java/com/epam/esm/entity/dto/TagDto.java
@@ -3,11 +3,15 @@ package com.epam.esm.entity.dto;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 @Data
 @NoArgsConstructor
 public class TagDto {
     private int id;
-    //TODO NotNull @Annotation
+    @NotNull
+    @Size(min = 2, max = 45)
     private String name;
 
     public TagDto(String name) {
